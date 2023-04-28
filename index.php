c4f28ea11dbd785865734185fdd67a2eacf172e7
Fehlling Halim <kevnhalim@gmail.com>
2023-04-29 01:55:14 +0700

    feat: add index

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Silaturahmi Online</title>
+</head>
+<body>
+    <h1 style="display: inline-block; position: relative; bottom: 20px;">Silaturahmi Online</h1>
+</body>
+</html>
