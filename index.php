225e14c6aa454e5bee28c79fd1cfa945949b8751
vronnn <zhafrandzaky@gmail.com>
2023-05-01 00:08:27 +0700

    feat: add landing page

--- index.php
+++ index.php
@@ -61,51 +61,89 @@
         </div>
         
         <div class='min-h-full z-0 absolute w-full -translate-y-14 flex flex-col gap-y-4 justify-center items-center'>
-            <h1 class='text-5xl font-bold drop-shadow-lg'>Silaturahmi Online</h1>
+            <h1 class='text-5xl font-bold drop-shadow-xl mb-2'>Silaturahmi Online</h1>
             <?php if ($messages) :?>
-            <table border="1" cellpadding="10" cellspacing="0" style="margin-left: auto; margin-right: auto;">
-                <tr>
-                    <th>No. </th>
-                    <th>Image</th>
-                    <th>Message</th>
-                    <th>Reply</th>
-                    <th>Action</th>
-                </tr>
+            <table border="3" cellpadding="10" cellspacing="3" style="margin-left: auto; margin-right: auto; width: 50%; font-family: 'Poppins', sans-serif;"
+                class="rounded-xl overflow-hidden text-center shadow-lg"
+            >
+                <colgroup>
+                    <col span={1} style="max-width: 5%">
+                    <col span={1} style="max-width: 20%">
+                    <col span={1} style="max-width: 30%">
+                    <col span={1} style="max-width: 30%">
+                    <col span={1} style="max-width: 10%">
+                </colgroup>
+                <thead class="bg-[#2A3553] text-[#FDFAF0] p-4 h-[50px]">
+                    <tr class="text-center">
+                        <th class="font-medium" style="font-family: 'Poppins', sans-serif;">No. </th>
+                        <th class="font-medium" style="font-family: 'Poppins', sans-serif;">Gambar</th>
+                        <th class="font-medium" style="font-family: 'Poppins', sans-serif;">Pesan</th>
+                        <th class="font-medium" style="font-family: 'Poppins', sans-serif;">Balasan</th>
+                        <th class="font-medium" style="font-family: 'Poppins', sans-serif;">Aksi</th>
+                    </tr>
+                </thead>
 
-                <?php $i = 1 ?>
+                <?php $i = $firstIndex+1 ?>
+                <tbody class="bg-white text-sm">
                 <?php foreach($messages as $m) :?>
-                    <tr>
-                        <td><?= $i++ ?></td>
-                        <td>
+                    <tr class="border-b h-[50px]">
+                        <td style="font-family: 'Poppins', sans-serif;"><?= $i++ ?></td>
+                        <td class="">
                             <?php if ($m['image']) :?>
-                                <img src="img/<?= $m['image'] ?>" width="50" height="50">
+                            <div class="w-full flex justify-center">
+                                <img src="img/<?= $m['image'] ?>" width="45" height="45">
+                            </div>
                             <?php else :?>
-                                <h4>None</h4>
+                                <h4 style="font-family: 'Poppins', sans-serif;">None</h4>
                             <?php endif; ?>
                         </td>
-                        <td><?= $m['content'] ?></td>
-                        <td>
+                        <td style="font-family: 'Poppins', sans-serif;" class="text-left max-w-[200px] truncate text-ellipsis overflow-x-hidden"><?= $m['content'] ?></td>
+                        <td style="font-family: 'Poppins', sans-serif;" class="text-left max-w-[10%] truncate text-ellipsis overflow-x-hidden">
                             <?php if ($m['reply']) :?>
                                 <p><?= $m['reply'] ?></p>
                             <?php else :?>
-                                <h4>Not replied yet</h4>
+                                <h4 style="font-family: 'Poppins', sans-serif;" class="text-center">Belum Dibalas</h4>
                             <?php endif; ?>
                         </td>
-                        <td>
-                            <a href="messages/edit.php?id=<?= $m["id"] ?>">Edit</a> |
-                            <a href="messages/delete.php?id=<?= $m["id"] ?>" 
+                        <td style="font-family: 'Poppins', sans-serif;" class="flex items-center justify-center">
+                            <a style="font-family: 'Poppins', sans-serif;" class="w-16 py-1 bg-[#FFF4DC] border border-[#99762C] text-[#99762C]" href="messages/edit.php?id=<?= $m["id"] ?>">Ubah</a>
+                            <div class=" w-1 h-full bg-slate-400"></div>
+                            <a style="font-family: 'Poppins', sans-serif;" class="w-16 py-1 bg-red-500 text-white ml-2" href="messages/delete.php?id=<?= $m["id"] ?>" 
                                 onclick="return confirm('Are you sure you want to delete this message?')">
-                                    Delete
+                                    Hapus
                             </a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
+                </tbody>
             </table>
+            <div class='flex items-center gap-x-2'>
+                <?php if ($currentPage > 1) :?>
+                    <a 
+                        href="?page=<?= $currentPage - 1 ?>">
+                            ←
+                    </a>
+                <?php endif; ?>
+
+                <?php for($i = 1; $i <= $pageCount; $i++) : ?>
+                    <a class=" w-8 h-8 rounded-md flex justify-center items-center bg-[#2A3553] bg-opacity-10 border border-[#2A3553] text-[#2A3553] backdrop-blur"
+                        href="?page=<?= $i ?>">
+                            <?= $i ?> 
+                    </a>
+                <?php endfor; ?>
+                
+                <?php if ($currentPage < $pageCount) :?>
+                    <a 
+                        href="?page=<?= $currentPage + 1 ?>">
+                            →
+                    </a>
+                <?php endif; ?>
+            </div>
         <?php else :?>
             <h2 class='font-medium text-lg text-center' style="font-family: 'Poppins', sans-serif;">Kamu belum membuat pesan apapun, ayo buat sekarang!</h2>
         <?php endif; ?>
             <a class='px-8 py-2 font-semibold uppercase text-white' href="messages/create.php" style="background: linear-gradient(143.45deg, #F79824 11.17%, #FDCA40 120.47%);
-border-radius: 8px 100px; font-family: 'Poppins', sans-serif;">buat pesan</a>
+            border-radius: 8px 100px; font-family: 'Poppins', sans-serif;">buat pesan</a>
         </div>
     </section>
     
